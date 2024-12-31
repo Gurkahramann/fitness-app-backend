f97460713b01344baaede948ee534ed301ebfbcb
Gurkahramannn <gurkan.kahraman@venhancer.com>
2024-12-31 20:22:31 +0300

    test

---
+++ src/test/java/com/fitnessapp/backend/fitnessapp/FitnessappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitnessapp.backend.fitnessapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
